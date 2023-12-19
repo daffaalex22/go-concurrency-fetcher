cd4793cd91a01546a410c321db56d6fc3cb52e07
Daffa' Alexander <daffaalex22@gmail.com>
2023-12-19 19:37:29 +0700

    Worker Pool Works, Write to JSON

--- main.go
+++ main.go
@@ -3,11 +3,17 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/url"
 	"strconv"
+	"sync"
 	"time"
 )
+
+type Job struct {
+	number int
+}
 
 type AnimeAttribute struct {
 	Synopsis       string `json:"synopsis"`
@@ -26,20 +32,78 @@ type Anime struct {
 	ID         string         `json:"id"`
 	Type       string         `json:"type"`
 	Attributes AnimeAttribute `json:"attributes"`
-	// Relationships map[string]interface{} `json:"-"`
 }
 
 type Body struct {
 	Data Anime `json:"data"`
 }
 
+var jobs = make(chan Job, 5)
+var results = make(chan Anime, 5)
+var URL = "/api/edge/anime/"
+var resultsCollection []Anime
+
 func main() {
-	URL := "/api/edge/anime/"
-	ID := 45857
+	const numOfJobs = 100
+	go allocateJobs(numOfJobs)
 
-	anime := animeFetcher(URL, ID)
+	resultsReceived := make(chan bool)
+	go getResults(resultsReceived)
 
-	fmt.Printf("%+v\n", anime)
+	const numOfWorkers = 5
+	createWorkerPool(numOfWorkers)
+
+	<-resultsReceived
+
+	resultsJSON, err := json.MarshalIndent(resultsCollection, "", " ")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	writeToFile(resultsJSON)
+}
+
+func writeToFile(json []byte) {
+	err := ioutil.WriteFile("results.json", json, 0644)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func allocateJobs(numOfJobs int) {
+	for i := 1; i <= numOfJobs; i++ {
+		jobs <- Job{i}
+	}
+
+	close(jobs)
+}
+
+func createWorkerPool(numOfWorkers int) {
+	var wg sync.WaitGroup
+	for i := 0; i < numOfWorkers; i++ {
+		go createWorker(&wg)
+		wg.Add(1)
+	}
+	wg.Wait()
+	close(results)
+}
+
+func createWorker(wg *sync.WaitGroup) {
+	for job := range jobs {
+		anime := animeFetcher(URL, job.number)
+
+		results <- anime
+	}
+
+	wg.Done()
+}
+
+func getResults(resultsReceived chan bool) {
+	for result := range results {
+		resultsCollection = append(resultsCollection, result)
+	}
+
+	resultsReceived <- true
 }
 
 func animeFetcher(Url string, id int) Anime {
@@ -56,14 +120,14 @@ func animeFetcher(Url string, id int) Anime {
 		Timeout: 5 * time.Minute,
 	}
 
-	response, error := client.Do(&request)
-	if error != nil {
-		fmt.Println(error)
+	response, err := client.Do(&request)
+	if err != nil {
+		fmt.Println(err)
 	}
 
 	var body Body
 
-	err := json.NewDecoder(response.Body).Decode(&body)
+	err = json.NewDecoder(response.Body).Decode(&body)
 	if err != nil {
 		fmt.Println(err)
 	}
